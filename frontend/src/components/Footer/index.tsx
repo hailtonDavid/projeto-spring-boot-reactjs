1f763f1970e8b34599fa0c4d598839a12d869ac4
hailtonDavid <hailton.david@gmail.com>
2021-09-08 16:37:21 -0300

    Basic statics components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,21 @@
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">
+                        <small>
+                            <strong>Todos os direitos reservados</strong>
+                            <br />
+                            <a href="http://analise-evolutiva.org" target="_blank" rel="noreferrer">
+                                Análise Evolutiva
+                            </a>
+                        </small>
+                    </p>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
