bd18ce006b07e8d80bd85a6df1bfef4c40c8dea2
hailtonDavid <hailton.david@gmail.com>
2021-09-09 16:25:11 -0300

    Layers

---
+++ backend/src/main/java/org/analiseevolutiva/dsvendas/respositories/SellerRepository.java
@@ -0,0 +1,8 @@
+package org.analiseevolutiva.dsvendas.respositories;
+
+import org.analiseevolutiva.dsvendas.entites.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+}
---
+++ backend/src/main/java/org/analiseevolutiva/dsvendas/services/SellerService.java
@@ -0,0 +1,24 @@
+package org.analiseevolutiva.dsvendas.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.analiseevolutiva.dsvendas.dto.SellerDTO;
+import org.analiseevolutiva.dsvendas.entites.Seller;
+import org.analiseevolutiva.dsvendas.respositories.SellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+
+}
